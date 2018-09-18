aeef88c34d367264625f5843cda520ebebecc529
ChinSinclair <sinclair_chin@hotmail.com>
2018-09-18 10:33:28 +1000

    Updated UI
    Updated Monthly Sales UI

--- monthlysales.php
+++ monthlysales.php
@@ -6,6 +6,7 @@
     <meta name="keywords" content="PHP" />
     <meta name="author" content="Name" />
     <link href="Bootstrap_4.0/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="style.css" rel="stylesheet" type="text/css" />
     <title>Page Title</title>
 </head>
 <body>
@@ -31,60 +32,63 @@
                     featuresList();
                 ?>
 
-                <!-- Generate Monthly Sales Report -->
-                <?php
-                    if (!isset($_POST["year"]) && !isset($_POST["month"])) {
-                        // if form data does not exist
-                        echo "Please select month and year to generate monthly report.";
-                    }
-                    else {
-                        // form data exists, obtain data from form through post method
-                        $year = $_POST["year"];
-                        $month = $_POST["month"];
-
-                        $username = "root";         // username to database admin
-                        $password = "";             // password to database admin
-                        $host = "localhost";        // localhost used to test query
-                        $database = "php";          // name of database in phpMyAdmin
-
-                        // connect to mysql database, localhost phpMyAdmin in this case
-                        $connection = new mysqli($host, $username, $password, $database);
-
-                        if ($connection->connect_error) {
-                            // if connection to database failed
-                            die ("Unable to connect" . $connection->connect_error);
+            </div>
+            <div class="col-lg-8 col-md-6">
+                <div id="form-section">
+                    <!-- Generate Monthly Sales Report -->
+                    <?php
+                        if (!isset($_POST["year"]) && !isset($_POST["month"])) {
+                            // if form data does not exist
+                            echo "Please select month and year to generate monthly report.";
                         }
                         else {
-                            // connection succeesful
-                            echo "Connection Successful";
-                        }
+                            // form data exists, obtain data from form through post method
+                            $year = $_POST["year"];
+                            $month = $_POST["month"];
 
-                        // query to obtain data based on user inputs of Year and Month
-                        $sql = "
-                        SELECT i.category_id, i.item_id, i.cost, i.retail_price, s.receipt_id, sold_time, sold_date, EXTRACT(YEAR FROM sold_date) AS Year, EXTRACT(MONTH FROM sold_date) AS Month, (i.retail_price - i.cost) AS Profit
-                        FROM Receipt AS r JOIN Sales AS s ON r.receipt_id = s.receipt_id JOIN Inventory AS i ON i.item_id = s.item_id
-                        WHERE (EXTRACT(YEAR FROM sold_date) = $year) AND (EXTRACT(MONTH FROM sold_date) = $month);
-                        ";
+                            $username = "root";         // username to database admin
+                            $password = "";             // password to database admin
+                            $host = "localhost";        // localhost used to test query
+                            $database = "php";          // name of database in phpMyAdmin
 
-                        // query to obtain gross profit
-                        $gross = "
-                        SELECT SUM(i.retail_price - i.cost) AS Gross_Profit
-                        FROM Receipt AS r JOIN Sales AS s ON r.receipt_id = s.receipt_id JOIN Inventory AS i ON i.item_id = s.item_id
-                        WHERE (EXTRACT(YEAR FROM sold_date) = $year) AND (EXTRACT(MONTH FROM sold_date) = $month);
-                        ";
+                            // connect to mysql database, localhost phpMyAdmin in this case
+                            $connection = new mysqli($host, $username, $password, $database);
 
-                        // submit query to database
-                        $result1 = mysqli_query($connection, $sql);
-                        $result2 = mysqli_query($connection, $gross);
+                            if ($connection->connect_error) {
+                                // if connection to database failed
+                                die ("Unable to connect" . $connection->connect_error);
+                            }
+                            else {
+                                // connection succeesful
+                                echo "Connection Successful";
+                            }
 
-                        // obtain row count from query results
-                        $rowcount1 = mysqli_num_rows($result1);
-                        $rowcount2 = mysqli_num_rows($result2);
-                ?>
+                            // query to obtain data based on user inputs of Year and Month
+                            $sql = "
+                            SELECT i.category_id, i.item_id, i.cost, i.retail_price, s.receipt_id, sold_time, sold_date, EXTRACT(YEAR FROM sold_date) AS Year, EXTRACT(MONTH FROM sold_date) AS Month, (i.retail_price - i.cost) AS Profit
+                            FROM Receipt AS r JOIN Sales AS s ON r.receipt_id = s.receipt_id JOIN Inventory AS i ON i.item_id = s.item_id
+                            WHERE (EXTRACT(YEAR FROM sold_date) = $year) AND (EXTRACT(MONTH FROM sold_date) = $month);
+                            ";
+
+                            // query to obtain gross profit
+                            $gross = "
+                            SELECT SUM(i.retail_price - i.cost) AS Gross_Profit
+                            FROM Receipt AS r JOIN Sales AS s ON r.receipt_id = s.receipt_id JOIN Inventory AS i ON i.item_id = s.item_id
+                            WHERE (EXTRACT(YEAR FROM sold_date) = $year) AND (EXTRACT(MONTH FROM sold_date) = $month);
+                            ";
+
+                            // submit query to database
+                            $result1 = mysqli_query($connection, $sql);
+                            $result2 = mysqli_query($connection, $gross);
+
+                            // obtain row count from query results
+                            $rowcount1 = mysqli_num_rows($result1);
+                            $rowcount2 = mysqli_num_rows($result2);
+                    ?>
 
                 <br />
 
-                <?php
+                    <?php
                         // if query is valid and row count is more than 0, which means data exists
                         if ($result1 && ($rowcount1 > 0)) {
                             // First table for monthly sales data
@@ -104,76 +108,72 @@
                                 </thead>
 
                                 <tbody>
-                            ";
-                            // fetch associate data based on query
-                            while ($row=mysqli_fetch_assoc($result1)) {
-                                // print related fields
+                                ";
+                                // fetch associate data based on query
+                                while ($row=mysqli_fetch_assoc($result1)) {
+                                    // print related fields
+                                    echo "
+                                        <tr>
+                                            <td>{$row['category_id']}</td>
+                                            <td>{$row['item_id']}</td>
+                                            <td>{$row['retail_price']}</td>
+                                            <td>{$row['receipt_id']}</td>
+                                            <td>{$row['sold_time']}</td>
+                                            <td>{$row['sold_date']}</td>
+                                            <td>{$row['Year']}</td>
+                                            <td>{$row['Month']}</td>
+                                            <td>{$row['Profit']}</td>
+                                        </tr>
+                                        ";
+                                }
                                 echo "
-                                    <tr>
-                                        <td>{$row['category_id']}</td>
-                                        <td>{$row['item_id']}</td>
-                                        <td>{$row['retail_price']}</td>
-                                        <td>{$row['receipt_id']}</td>
-                                        <td>{$row['sold_time']}</td>
-                                        <td>{$row['sold_date']}</td>
-                                        <td>{$row['Year']}</td>
-                                        <td>{$row['Month']}</td>
-                                        <td>{$row['Profit']}</td>
-                                    </tr>
-                                    ";
-                            }
-                            echo "
-                                </tbody>
-                            </table>
-                            ";
-                            // free result from query
-                            mysqli_free_result($result1);
+                                    </tbody>
+                                </table>
+                                ";
+                                // free result from query
+                                mysqli_free_result($result1);
                 ?>
 
                 <br />
 
-                <?php
-                            // Second table for gross profit
-                            // if query is valid and row count of gross profit is more than 0, which means data exists
-                            if ($result2 && ($rowcount2 > 0)) {
+                    <?php
+                                // Second table for gross profit
+                                // if query is valid and row count of gross profit is more than 0, which means data exists
+                                if ($result2 && ($rowcount2 > 0)) {
                                 // print table header
-                                echo "
-                                <table class='table table-striped table-bordered'>
-                                    <thead>
-                                        <th>Gross Profit</th>
-                                    </thead>
-
-                                    <tbody>
-                                ";
-                                // fetch associate data based on query
-                                while ($row=mysqli_fetch_assoc($result2)) {
-                                    // print gross profit
                                     echo "
-                                        <tr>
-                                            <td>{$row['Gross_Profit']}</td>
-                                        </tr>
-                                    ";
-                                }
-                                echo "
-                                        </tbody>
-                                    </table>
-                                ";
-                                // free result from query
-                                mysqli_free_result($result2);
-                            }
-                    }
-                        else {
-                            // if data does not exist in database
-                            echo "No data in database";
-                        }
-                        // close database connection
-                        mysqli_close($connection);
-                    }
-                        ?>
+                                        <table class='table table-striped table-bordered'>
+                                            <thead>
+                                                <th>Gross Profit</th>
+                                            </thead>
 
-            </div>
-            <div class="col-lg-8 col-md-6">
-                <div id="form-section">
+                                            <tbody>
+                                    ";
+                                    // fetch associate data based on query
+                                    while ($row=mysqli_fetch_assoc($result2)) {
+                                        // print gross profit
+                                        echo "
+                                            <tr>
+                                                <td>{$row['Gross_Profit']}</td>
+                                            </tr>
+                                        ";
+                                    }
+                                    echo "
+                                            </tbody>
+                                        </table>
+                                    ";
+                                    // free result from query
+                                    mysqli_free_result($result2);
+                                }
+                            }
+                            else {
+                                // if data does not exist in database
+                                echo "No data in database";
+                            }
+                            // close database connection
+                            mysqli_close($connection);
+                        }
+                    ?>
                 </div>
             </div>
         </div>
---
+++ monthlysalesUI.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="PHP Inc.: Page" />
+    <meta name="keywords" content="PHP" />
+    <meta name="author" content="Name" />
+    <link href="Bootstrap_4.0/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <title>Page Title</title>
+</head>
+<body>
+    <!--CODE HERE-->
+    <div id="navigation-bar">
+        <?php
+            include "include/template.php";
+            navigationBar();
+        ?>
+    </div>
+
+
+    <div id="webpage-title">
+        <?php
+            webpageTitle()
+        ?>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <?php
+                    featuresList();
+                ?>
+
+            </div>
+            <div class="col-lg-8 col-md-6">
+                <div id="form-section">
+                    <form action="monthlysales.php" method="post">
+                        <fieldset>
+                            <legend>Generate Monthly Sales Report</legend>
+                            <p><label>Enter Year: <input type="text" name="year" required /></label></p>
+                            <p>
+                                <label>Select month: </label>
+                                <select name="month" required>
+                                    <option value="">None</option>
+                                    <option value="01">January</option>
+                                    <option value="02">February</option>
+                                    <option value="03">March</option>
+                                    <option value="04">April</option>
+                                    <option value="05">May</option>
+                                    <option value="06">June</option>
+                                    <option value="07">July</option>
+                                    <option value="08">August</option>
+                                    <option value="09">September</option>
+                                    <option value="10">October</option>
+                                    <option value="11">November</option>
+                                    <option value="12">December</option>
+                                </select>
+                            </p>
+                            <p><input type="submit" value="Submit" /></p>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!--CODE HERE-->
+    <?php
+        jsPluggins();
+    ?>
+</body>
+</html>
